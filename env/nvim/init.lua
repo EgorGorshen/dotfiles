b0c584baa958a19c1196c9e4e0cd7a7a88f27f6c
EgorGorshen <egorgorshnckov@yandex.ru>
2025-08-22 14:33:17 +0300

    feat(nvim): start nvim setuping

--- env/nvim/init.lua
+++ env/nvim/init.lua
@@ -1,18 +1,3 @@
-vim.g.mapleader = ' '
-
-vim.keymap.set('i', 'jk', '<esc>')
-vim.keymap.set('n', '<leader>e', ':Ex<CR>')
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-vim.keymap.set('v', 'Y', "\"+y")
-
-vim.opt.mouse = 'a'
-vim.opt.number = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.cmd [[
-    highlight Normal guibg=none
-    highlight NoneText guibg=none
-]]
+-- NOTE: this file must be the first one, becouse I can not live without him
+require("core.main")
+require("lazynvim")
---
+++ env/nvim/lua/plugins/colors.lua
@@ -0,0 +1,31 @@
+function ColorMyPencils(color)
+	color = color or "rose-pine-moon"
+	vim.cmd.colorscheme(color)
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+end
+
+return {
+    {
+        "mathofprimes/nightvision-nvim",
+        name = "nightvision",
+        config = function()
+            ColorMyPencils("nightvision")
+        end
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        -- config = function()
+        --     require('rose-pine').setup({
+        --         disable_background = true,
+        --         styles = {
+        --             italic = false,
+        --         },
+        --     })
+        --
+        --     ColorMyPencils(name);
+        -- end
+    },
+}
